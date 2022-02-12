351281937f9ea4ff21a24d116da21315949c989e
santony <santony@nextgen.com>
2022-02-13 00:24:02 +0530

    Refactored

---
+++ ConsoleApp1/AllHPI.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class AllHPI
+    {
+        public Guid PersonId { get; set; }
+        public string ChiefComplaint1 { get; set; }
+        public string PopupName1 { get; set; }
+        public string Detail1 { get; set; }
+        public string ReasonForVisit_1 { get; set; }
+        public int IsFollowUp1 { get; set; }
+        public string CCNumber1 { get; set; }
+        public string ChiefComplaint2 { get; set; }
+        public string PopupName2 { get; set; }
+        public string Detail2 { get; set; }
+        public string ReasonForVisit_2 { get; set; }
+        public int IsFollowUp2 { get; set; }
+        public string CCNumber2 { get; set; }
+        public string ChiefComplaint3 { get; set; }
+        public string PopupName3 { get; set; }
+        public string Detail3 { get; set; }
+        public string ReasonForVisit_3 { get; set; }
+        public int IsFollowUp3 { get; set; }
+        public string CCNumber3 { get; set; }
+        public string ChiefComplaint4 { get; set; }
+        public string PopupName4 { get; set; }
+        public string Detail4 { get; set; }
+        public string ReasonForVisit_4 { get; set; }
+        public int IsFollowUp4 { get; set; }
+        public string CCNumber4 { get; set; }
+        public string ChiefComplaint5 { get; set; }
+        public string PopupName5 { get; set; }
+        public string Detail5 { get; set; }
+        public string ReasonForVisit_5 { get; set; }
+        public int IsFollowUp5 { get; set; }
+        public string CCNumber5 { get; set; }
+        public string ChiefComplaint6 { get; set; }
+        public string PopupName6 { get; set; }
+        public string Detail6 { get; set; }
+        public string ReasonForVisit_6 { get; set; }
+        public int IsFollowUp6 { get; set; }
+        public string CCNumber6 { get; set; }
+    }
+}
---
+++ ConsoleApp1/HpiDetail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class HpiDetail
+    {
+        public Guid PersonId { get; set; }
+        public int CcNumber { get; set; }
+        public string ChiefComplaint { get; set; }
+        public string PopupName { get; set; }
+        public string Detail { get; set; }
+        public string ReasonForVisit { get; set; }
+        public bool IsFollowUp { get; set; }
+        public bool IsDuplicate { get; set; }
+        public HpiType Type { get; set; }
+        public bool IsChronicCondition { get; set; }
+        public bool IsEnabled { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
